01b0aecdbd82f2f4cf8b937ae0c7c8702190b253
vivek_ <vivekkamandala@gmail.com>
2025-02-09 17:34:03 +0530

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,19 @@
+import {renderOrderSummary} from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
+
+
+new Promise((resolve) =>{
+loadProducts(() => { 
+    resolve();
+});
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+
+// loadProducts(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
