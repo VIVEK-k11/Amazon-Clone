e38effa71bc75ae9dc12ecf96b447b9b29e763fb
vivek_ <vivekkamandala@gmail.com>
2025-02-09 17:22:17 +0530

    Add files via upload

---
+++ data/deliveryOptions.js
@@ -0,0 +1,31 @@
+// DELIVERY OPTIONS FOR THE ITEMS IN THE CART
+export let deliveryOptions =[
+   {
+   id: '1',
+   deliveryDays: 7,
+   priceCents: 0
+   },
+   {
+    id: '2',
+    deliveryDays: 3,
+    priceCents: 499
+    },
+    {
+        id: '3',
+        deliveryDays: 1,
+        priceCents: 999
+    }
+];
+
+
+//FUNCTION TO GET THE OPTION BY THE OPTION ID
+export function getDeliveryOption(deliveryOptionId){
+    let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if(option.id === deliveryOptionId ){
+      deliveryOption = option;
+    }
+  });
+  return deliveryOption || deliveryOptions[0]; 
+}
